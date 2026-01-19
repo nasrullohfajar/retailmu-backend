59005effff0062c4b0785b2e7b2f7b73ccd9d57a
nasrullohfajar <nasrullohfajar10@gmail.com>
2026-01-19 13:59:15 +0700

    fix: resolve mongodb connection timeout

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,29 @@
-import express, { Request, Response, Application } from 'express';
-import dotenv from 'dotenv';
+import express from 'express';
 import cors from 'cors';
+import { errorHandler } from './middlewares/errorHandler';
+import categoryRoutes from './routes/master/category.route';
 
-dotenv.config();
+const app = express();
 
-const app: Application = express();
+const allowedOrigins = process.env.CLIENT_URL || 'http://localhost:5173';
 
-app.use(cors());
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('API is Running');
-});
+app.use('/api/categories', categoryRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.use(errorHandler);
+
+export default app;
