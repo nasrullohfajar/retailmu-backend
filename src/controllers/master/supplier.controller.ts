1c54cfc54a5ea040bcc0f97bf08f936d7643e4b1
nasrullohfajar <nasrullohfajar10@gmail.com>
2026-03-11 07:47:15 +0700

    feat: add supplier controller

---
+++ src/controllers/master/supplier.controller.ts
@@ -0,0 +1,100 @@
+import { Request, Response, NextFunction } from 'express';
+import * as supplierService from '../../service/master/supplier.service';
+import { successResponse } from '../../utils/successResponse';
+import { SUCCESS_MESSAGES } from '../../utils/message';
+
+export const create = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const supplier = await supplierService.createSupplier(req.body);
+
+    return successResponse(
+      res,
+      201,
+      SUCCESS_MESSAGES.CREATED('Supplier'),
+      supplier
+    );
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getAll = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const result = await supplierService.getAllSupplier(req.query);
+
+    return successResponse(
+      res,
+      200,
+      SUCCESS_MESSAGES.FETCHED('Supplier'),
+      result
+    );
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { id } = req.params;
+    const supplier = await supplierService.getSupplierById(id as string);
+
+    return successResponse(
+      res,
+      200,
+      SUCCESS_MESSAGES.FETCHED('Supplier'),
+      supplier
+    );
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const update = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { id } = req.params;
+    const supplier = await supplierService.updateSupplier(
+      id as string,
+      req.body
+    );
+
+    return successResponse(
+      res,
+      200,
+      SUCCESS_MESSAGES.UPDATED('Supplier'),
+      supplier
+    );
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const remove = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { id } = req.params;
+    await supplierService.deleteSupplier(id as string);
+
+    return successResponse(res, 200, SUCCESS_MESSAGES.DELETED('Supplier'));
+  } catch (error) {
+    next(error);
+  }
+};
