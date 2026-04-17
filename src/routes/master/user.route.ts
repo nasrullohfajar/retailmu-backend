b55b7d376f8ab317ef0d6cc0651e41c1d24033fb
nasrullohfajar <nasrullohfajar10@gmail.com>
2026-04-17 14:26:15 +0700

    feat: add api protection

--- src/routes/master/user.route.ts
+++ src/routes/master/user.route.ts
@@ -2,13 +2,41 @@ import { Router } from 'express';
 import * as userController from '../../controllers/master/user.controller';
 import { validate } from '../../middlewares/validate';
 import { createUserSchema } from '../../validations/master/user.validation';
+import { protect } from '../../middlewares/auth.middleware';
+import { restrictTo } from '../../middlewares/role.middleware';
 
 const router = Router();
 
-router.post('/', validate(createUserSchema), userController.create);
-router.get('/', userController.getAll);
-router.get('/:id', userController.getById);
-router.put('/:id', validate(createUserSchema), userController.update);
-router.delete('/:id', userController.remove);
+router.post(
+  '/',
+  protect,
+  restrictTo('administrator'),
+  validate(createUserSchema),
+  userController.create
+);
+
+router.put(
+  '/:id',
+  protect,
+  restrictTo('administrator'),
+  validate(createUserSchema),
+  userController.update
+);
+
+router.delete(
+  '/:id',
+  protect,
+  restrictTo('administrator'),
+  userController.remove
+);
+
+router.get('/', protect, restrictTo('administrator'), userController.getAll);
+router.get('/me', protect, userController.me);
+router.get(
+  '/:id',
+  protect,
+  restrictTo('administrator'),
+  userController.getById
+);
 
 export default router;
